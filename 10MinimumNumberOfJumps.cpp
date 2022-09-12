aac1c58f47bb69f344dcffb54547307057b84ee7
Abhishek <abhishekchauhan85805@gmail.com>
2022-09-12 15:38:06 +0530

    10MinimumNumberOfJumps.cpp corrected

--- 10MinimumNumberOfJumps.cpp
+++ 10MinimumNumberOfJumps.cpp
@@ -4,19 +4,56 @@ using namespace std;
 
 class Solution{
     public:
-        int minJumps(vector<int> &v, int size){
-            int i, j=0, count=0, prev=0;
-            for(i=0;i<size;i++){
-                j+=v[i];
-                count++;
-                if(j>=size)
-                    return count;
-                if(j==prev)
-                    return -1;
-                prev=j;
+        int min(int a, int b){
+            if(a>b)
+                return b;
+            return a;
+        }
+
+        int max(int a, int b){
+            if(a>b)
+                return a;
+            return b;
+        }
+
+        int minJumpsDP(vector<int> v, int size){
+            int jumps[size], i, j;
+            jumps[0] = 0;
+            if(size == 0 || v[0] == 0)
+                return INT16_MAX;
+            for(i=1;i<size;i++){
+                jumps[i] = INT16_MAX;
+                for(j=0;j<i;j++){
+                    if(i<=v[j]+j && v[j] != INT16_MAX){
+                        jumps[i] = min(jumps[i], jumps[j]+1);
+                        break;
+                    }
+                }
+            }
+            return jumps[size-1];
+        }
+
+        int minJumpsGreedy(vector<int> v, int size){
+            int jumps = 1, steps = v[0], maxreach = v[0], i;
+            if(size<=1)
+                return 0;
+            if(v[0]==0)
+                return -1;
+            for(i=1;i<size;i++){
+                if(i==size-1)
+                    return jumps;
+                maxreach = max(maxreach, i+v[i]);
+                steps--;
+                if(steps==0){
+                    jumps++;
+                    if(i>=maxreach)
+                        return -1;
+                    steps = maxreach-i;
+                }
             }
             return -1;
         }
+
 };
 
 int main(){
@@ -28,7 +65,7 @@ int main(){
         v.push_back(element);
     }
     Solution obj;
-    cout<<obj.minJumps(v, size);
+    cout<<obj.minJumpsDP(v, size)<<" "<<obj.minJumpsGreedy(v, size);
     
     return 0;
 }
